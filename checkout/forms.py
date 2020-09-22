1cf71520b88f7af93925e67e9f8fbc092a8d3819
Matt <matt.snell.00@hotmail.co.uk>
2020-09-22 14:06:25 +0000

    added template views and templates

--- checkout/forms.py
+++ checkout/forms.py
@@ -3,30 +3,32 @@ from .models import Order
 
 
 class OrderForm(forms.ModelForm):
-    model = Order
-    fields = ('full_name', 'email', 'phone_number',
-              'street_address1', 'street_address2',
-              'town_or_city', 'postcode',
-              'country', 'county')
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
 
     def __init__(self, *args, **kwargs):
-        """ Add placeholders and classes, remove auto-generated
-        lables and set autofocus on first field """
-
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
         super().__init__(*args, **kwargs)
         placeholders = {
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
             'country': 'Country',
-            'postcode': 'Postcode',
+            'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
             'county': 'County',
         }
 
-        self.fields['full_name'].widhet.attrs['autofocus'] = True
+        self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if self.fields[field].required:
                 placeholder = f'{placeholders[field]} *'
